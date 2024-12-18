826fc8778a46b6229d4c3f70bf0c3c9825ae846e
Emmanuelle Coutu-Nadeau <ecnadeau@tactiohealth.com>
2024-12-18 17:41:39 -0500

    Saving SNR in a CSV

--- EEG_visuals.py
+++ EEG_visuals.py
@@ -1,18 +1,23 @@
+# Date created: 12/18/2024
+# Author: Emmanuelle CN
+
 import mne
 import numpy as np
 import matplotlib.pyplot as plt
 from mne.preprocessing import find_bad_channels_maxwell
 
 
-# Load your EEG data
-file_path = 'Data/6Hz/Q1K_HSJ_1025-1042_F1_AEP_A_20240715_095058_processed_ae06.set'
+# Load  EEG data
+file_path = 'file_path.set'
 epochs = mne.io.read_epochs_eeglab(file_path)
 
 # Check for any existing bad channel annotations in the data
+# TODO need to identify bad channels to simplify future analysis
 print("Bad channels:", epochs.info['bads'])
 
 # Plot the epochs data
-epochs.plot(n_epochs=10, n_channels=len(epochs.ch_names), 
+epochs.plot(n_epochs=10, 
+            n_channels=len(epochs.ch_names),  # Change the number of epochs to view different parts of the EEG file
             scalings='auto')
 
 # Plot spectral density
